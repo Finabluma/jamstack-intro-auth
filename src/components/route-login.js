570e5de1cce3d03badd67481e0c537d588133ef7
Finabluma <oscar.francino@finabluma.com>
2020-11-17 17:38:19 +0100

    project setup

---
+++ src/components/route-login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { navigate } from 'gatsby';
+
+const RouteLogin = () => {
+
+  return (
+    <>
+      <h1>Log In or Sign Up</h1>
+      <button>Login</button>
+    </>
+  );
+};
+
+export default RouteLogin;
